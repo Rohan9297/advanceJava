f86e78da7b42e2ae13020ee02884024998d8999b
Rohan <rohankolekar66@gmail.com>
2025-09-18 22:24:34 +0530

    new leetcode 720 solved

---
+++ src/DataStrucure/HashMap/Leetcode720.java
@@ -0,0 +1,39 @@
+package DataStrucure.HashMap;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Leetcode720 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String[] words = { "w", "wo", "wor", "worl", "world" };
+        String str = s.longestWord(words);
+        System.out.println(str);
+    }
+}
+
+class Solution {
+
+    public String longestWord(String[] words) {
+
+        Arrays.sort(words);
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+
+        String str = "";
+        Set<String> set = new HashSet<>();
+        for (String st : words) {
+
+            if (st.length() == 1 || set.contains(st.substring(0, st.length() - 1))) {
+                set.add(st);
+
+                if (st.length() == str.length() && st.compareTo(str) < 0 || st.length() > str.length()) {
+                    str = st;
+                }
+            }
+
+        }
+        return str;
+
+    }
+}
